b6523657da04652a34c6c879337844532b55f0d4
ezenjun <ezenjun07@gmail.com>
2023-01-03 21:59:08 +0900

    ✨ 카카오 친구목록 가져오기 팀원 추가해야 진행가능

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,7 +1,28 @@
-import React from "react";
-
 const Home = () => {
-	return <div>Home</div>;
+	const token = localStorage.getItem("token");
+	const getFriendList = () => {
+		fetch(`https://kapi.kakao.com/v1/api/talk/friends?`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data) {
+					alert("성공");
+					console.log(data);
+				} else {
+					alert("실패");
+				}
+			});
+	};
+	return (
+		<div>
+			<div>Home</div>
+			<button onClick={getFriendList}>Friend List</button>
+		</div>
+	);
 };
 
 export default Home;
--- client/src/Pages/Login/OAuth.js
+++ client/src/Pages/Login/OAuth.js
@@ -1,3 +1,3 @@
 export const REST_API_KEY = "55b1930f9704c1df22dcdd26326d11a1";
 export const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=friends`;
