20196e65fdee908ad9b0641ffd7d4171d8578868
ezenjun <ezenjun07@gmail.com>
2023-02-06 17:22:30 +0900

    ✨ feat: 초기 설정 page 진행중 welcome, setUserName,setRoomName

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import styled from "styled-components";
 const Landing = () => {
 	let navigate = useNavigate();
 	useEffect(() => {
@@ -8,9 +8,25 @@ const Landing = () => {
 		if (token === "yes") {
 			navigate("/login");
 		}
-	}, []);
+	});
 
-	return <div>Landing</div>;
+	return (
+		<Container>
+			{/* image */}
+			<div>Landing</div>
+			{/* login */}
+			<div>
+				<button onClick={() => navigate("/login")}>login</button>
+			</div>
+		</Container>
+	);
 };
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	height: 100vh;
+`;
 
 export default Landing;
